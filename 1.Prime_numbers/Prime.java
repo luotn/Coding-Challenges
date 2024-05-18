2212d0f726a44a1d4ba118e2bc8bc61f3474572d
luotn <luotn@hotmail.com>
2024-05-18 08:29:31 +0100

    Completed prime number finder.

--- 1.Prime_numbers/Prime.java
+++ 1.Prime_numbers/Prime.java
@@ -5,7 +5,7 @@ public class Prime{
     public static void main(String[] args) {
         long startTime = System.nanoTime();
         int min = 0;
-        int max = 10000;
+        int max = 100;
 
         System.out.println(checkPrime(min, max));
         
@@ -36,7 +36,7 @@ public class Prime{
         // the given value to find all non-prime 
         // number components.
         for(int j = 2; j <= Math.sqrt(i); j++) {
-        // for(int j = 2; j < i; j++) {
+        // for(int j = 2; j < i; j++) {     #origional
             if (i % j == 0)
             {
                 result = false;
